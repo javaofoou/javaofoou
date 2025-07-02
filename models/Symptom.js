73d594381cd9e016996edd317cf67136aed797a4
javaofoou <almaslahatilislamiyat@gmail.com>
2025-07-02 19:31:20 +0100

    Update Symptom.js

--- models/Symptom.js
+++ models/Symptom.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 const symptomSchema = new mongoose.Schema({
   illness: String,           // e.g. "Flu"
   symptoms: [String],          // e.g. ["fever", "cough"]
-  treatment: String            // e.g. "Paracetamol and rest"
+  treatment: String,
+  recommendation: String,
+  dosage: String // e.g. "Paracetamol and rest"
 });
 
 export const Symptom = mongoose.model("Symptom", symptomSchema);
